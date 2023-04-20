fae525991dbd9c5c6413f20190d11f1335e85210
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-04-20 15:00:49 -0300

    Criando Autenticação

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -1,8 +1,11 @@
+
 import { adminJsResources } from './resources/index';
 import AdminJS from "adminjs"
 import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from './../database/seeders/index';
+import { User } from "../models"
+import bcrypt from "bcrypt"
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -11,7 +14,7 @@ export const adminJs = new AdminJS({
     rootPath: "/admin",
     resources: adminJsResources,
     branding: {
-        companyName: 'EducaPlayTec',
+        companyName: 'Educa',
         logo: '/onebitflix.svg',
         theme: {
         colors: {
@@ -33,4 +36,28 @@ export const adminJs = new AdminJS({
 }
 })
 
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
+//Esse código é para habilitar a autenticação dos logins
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    authenticate: async (email, password) => {
+        const user = await User.findOne({ where: { email }})
+
+        if (user && user.role === 'admin') {
+            // Se a senha é igual á cadastrada
+            const matched = await bcrypt.compare(password, user.password)
+
+            if (matched) {
+                return user
+            }
+        }
+
+        // Se não for
+        return false
+    },
+    // Aqui captura as senha salvas
+    cookiePassword: 'senha-do-cookie'
+},
+// Isso aqui é para a IDE entender q estou usando bibliotecas desatualizada, pq a antiga está melhor que a nova
+null, {
+    resave: false,
+    saveUninitialized: false
+})
