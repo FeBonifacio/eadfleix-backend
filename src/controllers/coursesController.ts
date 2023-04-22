36159090f72c20740274d1b3c7bfc7ee90657621
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-04-22 13:00:07 -0300

    Buscando curso pelo nome

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { courseService } from './../services/coursesService';
 import { Request, Response } from 'express';
+import { getPaginationParams } from '../helpers/getPaginationParams';
 
 
 //Faz a mesma coisa do categoriesController, busca de forma organizada para o front-end
@@ -33,6 +34,23 @@ export const coursesController = {
         }
     },
 
+    // GET /courses/search?name=
+    search: async (req: Request, res: Response) => {
+        const { name } = req.query
+        const [page, perPage] = getPaginationParams(req.query)
+
+        //Aqui é o metodo do controlador
+        try {
+            if (typeof name !== 'string') throw new Error('name param must be of type string')
+            const courses = await courseService.findByName(name, page, perPage)
+            return res.json(courses)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            } 
+        }
+    }, 
+
     // GET /courses/:id
     show: async (req: Request, res: Response) => {
         const { id } = req.params
@@ -46,6 +64,5 @@ export const coursesController = {
             } 
         }
     },  
-
 
 }
